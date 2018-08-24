d46203ad3e11edce1f1d5a89ef99589db3fd935c
jgp <948102903@qq.com>
2018-08-24 20:26:29 +0800

    wx test

---
+++ entity/Misic.java
@@ -0,0 +1,25 @@
+package com.wx.ioc.wxdemo.entity;
+
+/**
+ * @ProjectName: wxdemo
+ * @Package: com.wx.ioc.wxdemo.entity
+ * @ClassName: Misic
+ * @Description: java类作用描述
+ * @Author: 焦关平
+ * @CreateDate: 2018/8/24 20:25
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2018/8/24 20:25
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class Misic {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
--- test/Suijishu.java
+++ test/Suijishu.java
@@ -14,6 +14,9 @@ package com.wx.ioc.wxdemo.test;
  */
 public class Suijishu {
     public static void main(String[] args) {
-
+        String a = "1";
+        String b = "1";
+        String c = "11";
+        System.out.println(c.equals(a+b));
     }
 }
